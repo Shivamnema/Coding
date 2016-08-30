ce7e869b288f4ea763ab6129da3e1bc417e87c86
Shivam Nema <nemashivam5@gmail.com>
2016-08-30 22:14:23 +0530

    30th Aug 10:14PM

---
+++ threewaycomminucation.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+int main(void) {
+    int T;
+    cin>> T;
+    while (T >= 0)
+    {   
+        cout<< "hello";
+        T--;
+    }
+
+    return 0;
+}
