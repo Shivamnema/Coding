0b0fa9bca5195f77e64292af68acf5486622d2d6
Shivam Nema <nemashivam5@gmail.com>
2016-08-31 20:26:41 +0530

    commit at aug 31, 2016 :: 8:26PM

---
+++ smallfactorial.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <list>
+#include <algorithm>
+
+int factorial (int);
+
+//factorial problem codechef. Date : 31th AUG 2016. 
+//Shivamnema05
+using namespace std;
+
+int main(void) {
+    int T; //test cases
+    
+    cin>>T;
+    int fact = 0;
+    
+    int num;
+    int arr[1000] = {1};
+    int carry = 0;
+
+    
+    while (T--)
+    {
+        cin >> num;
+
+        for (int i = 1; i <= num ; i++){
+            
+        }
+    }
+    return 0;
+}
+
