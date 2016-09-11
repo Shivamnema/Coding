4c1f1140d97612558de682ebcd8138e0d5bc3a98
Shivam Nema <nemashivam5@gmail.com>
2016-09-11 23:03:25 +0530

    version control system problem solution

--- VCS.cpp
+++ VCS.cpp
@@ -9,16 +9,35 @@
 //Shivamnema05
 using namespace std;
 
-
+int N; //no of source files
+int M ; //no of ignored files
+int K; //no of tracked files
 int main(void) {
-    int T;  
+    int T, i;  
     cin >>T;
     while(T--){
-        int N, M, K;
+        int scrfile[100] = {0};
         cin >>N >>M >>K;
-        int  A[M];
-        int  B[K];
-
+        int tracked, untracked,val;
+        
+        for (i=0 ; i<M ; i++ ){
+            cin >> val;   
+            scrfile[val]++;
+        }
+                
+        for (i =0 ; i<K ; i++ ){
+            cin >> val;
+            scrfile[val]++;
+        }
+        for (i=1 ; i<=N ; i++){
+            if(scrfile[i] == 2){
+                tracked++;
+            }else if(scrfile[i] == 0) {
+                untracked++;
+            }
+        }
+        cout<<tracked<<" "<<untracked<<endl;
+        
     }
     return 0;
 }
